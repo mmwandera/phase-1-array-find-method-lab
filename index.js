573a5e1585139bc188d7006c10cec198a24b1f48
mmwandera <mmwandera74@gmail.com>
2023-10-02 16:30:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin(array){
+    const yearWon = array.find(function(element){
+        return element.result === "W"
+    })
+
+    if(yearWon){
+        return yearWon.year
+    } else {
+        return undefined
+    }
+}
